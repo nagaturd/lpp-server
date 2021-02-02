30e75be26b43adf83066567ed4b0c6660114cc85
kibumah <thebriankwon@gmail.com>
2021-02-02 17:45:28 -0500

    Migration to typescript

--- src/api/summoner.ts
+++ src/api/summoner.ts
@@ -1,24 +1,23 @@
-import express from 'express'
-import fetch from 'node-fetch'
+import express from "express";
+import fetch from "node-fetch";
 
-const router = express.Router()
+const router = express.Router();
 
 const BASE_URL =
-  'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
+  "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
 
-router.get('/:name', async (req, res) => {
+router.get("/:name", async (req, res) => {
   try {
-    const summoner = req.params.name
-    const apiKey = process.env.RIOT_API_KEY
+    const summoner = req.params.name;
+    const apiKey = process.env.RIOT_API_KEY;
 
-    const response = await fetch(`${BASE_URL}${summoner}?api_key=${apiKey}`)
-    const data = await response.json()
-    console.log(data)
+    const response = await fetch(`${BASE_URL}${summoner}?api_key=${apiKey}`);
+    const data = await response.json();
 
-    res.json(data)
+    res.json(data);
   } catch (err) {
-    res.send('error')
+    res.send("error");
   }
-})
+});
 
-export default router
+export default router;
